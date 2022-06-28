63448bb448a2e9e242cd17b86b363152342c3dae
L-Karimi <luciahkei101@gmail.com>
2022-06-28 07:32:35 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,49 @@
+// Assignment 1:
+// Conditionals & Loops1. Write a JavaScript program which iterates the integers from 1 to 100. But for
+// multiples of three print "Fizz" instead of the number and for the multiples of five print
+// "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+// 2. Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
+// 3. Write a JavaScript for loop that will iterate from 0 to 20. For each iteration, it will
+// check if the current number is odd or even, and display a message to the screen.
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+// 4. Write a JavaScript conditional statement to find the largest of five numbers in the
+// given array.
+// Let num = [-2, 4,-5, 6,0]
+// 5. Using conditional statements, write a JavaScript program to find the largest of the
+// following two numbers: 10 & 40
+// 6. Write a JavaScript program to find leap years from 2000 to 2022
+
+
+function fizzbuzz(){
+for (var i = 0; i < 101; i++) {
+  if (i % 15 == 0) console.log("Fizz");
+  else if (i % 3 == 0) console.log("Buzz");
+  else if (i % 3 == 0) console.log("FizzBuzz");
+  console.log(i);
+}
+
+}
+
+function multiplications() {
+  for (var x = 0; x < 101; x++) {
+    if (x % 3 == 0 || x % 5 == 0) sum += 1;
+  }
+  return sum;
+}
+console.log(sum);
+
+
+function message(){
+    for (var z=0;z<21;z++){
+        if (z%2==0){
+            console.log("${z} is even");
+        }
+    else{
+        console.log("${z} is odd");
+
+    }
+}
+}
